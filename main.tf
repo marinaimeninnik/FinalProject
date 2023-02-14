d58a9fa255c422d1ab23beddac46497c60fd8c63
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-14 19:01:39 +0100

    some changes into soft installation

--- main.tf
+++ main.tf
@@ -100,13 +100,13 @@ resource "aws_instance" "Ubuntu2004" {
       "sudo apt update",
       "sudo apt-get -y install apache2",
     # "sudo mkdir /var/www/php/",
-      "sudo apt-get -y install mysql-server",
-      "sudo systemctl start mysql.service",
-      "git clone https://github.com/marinaimeninnik/FinalProject.git",
-      "sudo rm /var/www/html/index.html",
-      "sudo cp /home/ubuntu/FinalProject/HTML/index.php /var/www/html",
-      "sudo cp /home/ubuntu/FinalProject/HTML/127_0_0_1_1.sql /var/www/html",
-      "sudo cp /home/ubuntu/FinalProject/HTML/connect.php /var/www/html"
+      # "sudo apt-get -y install mysql-server",
+      # "sudo systemctl start mysql.service",
+      # "git clone https://github.com/marinaimeninnik/FinalProject.git",
+      # "sudo rm /var/www/html/index.html",
+      # "sudo cp /home/ubuntu/FinalProject/HTML/index.php /var/www/html",
+      # "sudo cp /home/ubuntu/FinalProject/HTML/127_0_0_1_1.sql /var/www/html",
+      # "sudo cp /home/ubuntu/FinalProject/HTML/connect.php /var/www/html"
 
     #   "mkdir data",
     #   "cd data",

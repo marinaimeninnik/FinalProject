b3463c99cc0b890d5ae4278786b146389a20fbdc
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-15 19:28:12 +0100

    overwritted in new style

--- HTML/connect.php
+++ HTML/connect.php
@@ -1,24 +1,32 @@
 <?php
-    
-    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-        $conn= mysqli_connect('database1.cm6htjqaiy1e.us-east-1.rds.amazonaws.com', 'maynaDB', '12345678', 'database_1') or die ("Connection Failed:" .mysql_connect_error());
-        if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
-            $name= $_POST['name'];
-            $email= $_POST['email'];
-            $phone= $_POST['phone'];
-            $bgroup= $_POST['bgroup'];
 
-            $sql= "INSERT INTO `users` (`name`, `email`, `phone`, `bgroup`)  VALUES ('$name', '$email', '$phone', '$bgroup')";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$bgroup = $_POST['bgroup'];
 
-            $query = mysqli_query($conn,$sql);
-            if($query) {
-                echo 'Entry Successfull';
-            }
-            else {
-                echo 'Error Occurred';
-            }
-        }
-        $conn->close();
-    }
+$servername = "database1.cm6htjqaiy1e.us-east-1.rds.amazonaws.com";
+$username = "maynaDB";
+$password = "12345678";
+$dbname = "database_1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Chaeck connection
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql= "INSERT INTO `users` (`name`, `email`, `phone`, `bgroup`)  VALUES ('$name', '$email', '$phone', '$bgroup')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
 
 ?>

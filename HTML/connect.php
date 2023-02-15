8c58951b0e5c26c0c614f0256ac9da2dc445d012
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-15 20:52:22 +0100

    test

--- HTML/connect.php
+++ HTML/connect.php
@@ -2,7 +2,7 @@
     
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
         echo "Test1";
-        $conn= mysqli_connect('localhost', 'root', '123456', 'database_1') or die ("Connection Failed:" .mysql_connect_error());
+        $conn= mysqli_connect('localhost', 'root', '', 'database_1') or die ("Connection Failed:" .mysql_connect_error());
         echo "Test2";
         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
             $name= $_POST['name'];

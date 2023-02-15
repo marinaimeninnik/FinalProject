cfba4423104a5f25a652df32026525675eff702c
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-15 19:58:30 +0100

    test

--- HTML/connect.php
+++ HTML/connect.php
@@ -13,10 +13,13 @@ $dbport = "3306";
 
 // Create connection
 //$link = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+echo "test1";
 $conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+echo "test 2";
 
 // Chaeck connection
 if ($conn->connect_error) {
+    echo "test3";
     die("Connection failed: ". $conn->connect_error);
 }
 echo "Connected successfully";

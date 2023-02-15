86c0c39fd6e3a699b9406dc5898bc55f519bf418
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-15 15:56:34 +0100

    test

--- HTML/connect.php
+++ HTML/connect.php
@@ -1,6 +1,14 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-        $conn= mysqli_connect('database1.cm6htjqaiy1e.us-east-1.rds.amazonaws.com', 'maynaDB', '12345678', 'database_1') or die ("Connection Failed:" .mysqli_connect_error());
+    
+    echo "test1";
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) 
+    
+    echo "test2";
+
+    {
+
+        $conn= mysqli_connect('database1.cm6htjqaiy1e.us-east-1.rds.amazonaws.com', 'maynaDB', '12345678', 'database_1') or die ("Connection Failed:" .mysql_connect_error());
         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
             $name= $_POST['name'];
             $email= $_POST['email'];
@@ -16,6 +24,8 @@
             else {
                 echo 'Error Occurred';
             }
+        // mysql_close($conn);
         }
     }
+
 ?>

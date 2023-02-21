0100c7d597387536dcecbd483153feb7adc80ec0
HP-Pavillion-Laptop <marinaimeninnik@gmail.com>
2023-02-21 15:42:39 +0100

    test

--- HTML/SamplePage.php
+++ HTML/SamplePage.php
@@ -2,7 +2,7 @@
 <?php include "inc/dbinfo.inc"; ?>
 <html>
 <body>
-<h1>Sample page YES</h1>
+<h1>Sample page Test</h1>
 <?php
   $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
